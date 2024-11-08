05a0217a63db75fe97fb5d923d97bb9991f10c40
Andy Ly <aly37@ucsc.edu>
2024-11-07 23:34:40 -0800

    fixed tt_um_lif.v

--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -17,9 +17,8 @@ module tt_um_lif (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out [6:0] = 0;
+  assign uio_oe  = 1;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
