119bd28001a6429c0b8e358fa77fee1cb8965a47
Andy Ly <aly37@ucsc.edu>
2024-11-07 23:22:11 -0800

    fix

--- src/lif.v
+++ src/lif.v
@@ -1,4 +1,4 @@
-`default_nettype
+`default_nettype none
 
 module lif (
     input wire [7:0]    current,
@@ -6,7 +6,7 @@ module lif (
     input wire          reset_n,
     output reg [7:0]    state,
     output wire         spike,
-)
+);
 
     wire [7:0] next_state;
     reg [7:0] threshold;
